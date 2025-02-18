9ca7c406dc1495d1294490d395fc52de0d365f64
Estebanr32 <juanes020609@gmail.com>
2025-02-17 21:48:33 -0500

    Avance calculator

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 print ("Hola, bienvenido a Calculator.git")
 
 operacion = int(input("Ingrese la operación que desea usar:\n1.Suma \n2.Resta \n3.Multiplicación \n4.División \n"))
+
+def suma (a,b): 
+    result = a + b
+    return result
+
+def resta (a,b):
+    result = a - b
+    return result
